0e88c10161d1aa01ba95cfcdf7dbcde1ca545281
EC2 Default User <ec2-user@ip-172-31-30-36.us-west-2.compute.internal>
2019-11-16 00:40:04 +0000

    Initial commit: * Sets up node-resque with worker, scheduler, and queues * PoC fetch nytbee puzzle and parse data

---
+++ src/helpers/puzzleDataParser.js
@@ -0,0 +1,62 @@
+const parsePuzzleData = (puzzleHTML) => {
+  const validWords = getValidWords(puzzleHTML);
+  
+  let puzzleLetters = []
+  validWords.map(word => {
+    const lettersInWord = word.split('');
+    for (let letter of lettersInWord) {
+      puzzleLetters.push(letter);
+    }
+    
+    return null;
+  });
+  puzzleLetters = [...new Set(puzzleLetters)]
+  
+  const centerLetter = getCenterLetter(validWords, puzzleLetters);
+  const outerLetters = getOuterLetters(centerLetter, puzzleLetters);
+  
+  const pointsNeededForGenius = getPointsNeededForGenius(puzzleHTML);
+  
+  return {
+    validWords: validWords,
+    centerLetter: centerLetter,
+    outerLetters: outerLetters,
+    pointsNeededForGenius: pointsNeededForGenius,
+  }
+}
+
+const getValidWords = (puzzleHTML) => {
+  let wordsListString = puzzleHTML.match(/"words":\[\[(.*?)\]\]/)[0];
+  return wordsListString.match(/\w+/g).slice(1);
+}
+
+const getCenterLetter = (validWordsList, puzzleLetters) => {
+  let centerLetterCandidates = puzzleLetters.map(letter => letter);
+  
+  for (const word of validWordsList) {
+    for (const letter of centerLetterCandidates) {
+      if (word.indexOf(letter) === -1) {
+        const indexForLetterToRemove = centerLetterCandidates.indexOf(letter); 
+        centerLetterCandidates.splice(indexForLetterToRemove, 1);
+      }
+    }
+  }
+  
+  return centerLetterCandidates[0];
+}
+
+const getOuterLetters = (centerLetter, puzzleLetters) => {
+  let letters = puzzleLetters.map(letter => letter);
+  const centerLetterIndex = puzzleLetters.indexOf(centerLetter);
+  letters.splice(centerLetterIndex, 1);
+  
+  return letters;
+}
+
+const getPointsNeededForGenius = (puzzleHTML) => {
+  return Number(puzzleHTML.match(/Points Needed for Genius: (\d*)/)[1]);
+}
+
+module.exports = {
+  parsePuzzleData
+}
